ae3042c46f7140caf00604e404a9d6c2ddb37b1d
chano212 <chance@thechanceproject.org>
2020-10-14 16:33:26 -0700

    added navbar and page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        Home page yo
-      </Route>
+    <div>
+      <Navs />
 
-      <Route exact path="/starred">
-        Starred page yo
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route>Not a page yo</Route>
-    </Switch>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>Not a page yo</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Starred = () => {
+  return <div>Starred page yo</div>;
+};
+
+export default Starred;
