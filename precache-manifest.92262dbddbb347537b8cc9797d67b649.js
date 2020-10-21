8eba55a849a780521e603f4a73f8176c61477862
chano212 <chance@thechanceproject.org>
2020-10-20 21:17:34 -0700

    Updates

---
+++ precache-manifest.92262dbddbb347537b8cc9797d67b649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b4f984049c65e7fe74ef40a2f9305f",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "977d5a076884ca15a8f4",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "fe0009c4d8130d936365",
+    "url": "/box-office/static/js/2.b6690efa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.b6690efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977d5a076884ca15a8f4",
+    "url": "/box-office/static/js/main.c3606fcf.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
